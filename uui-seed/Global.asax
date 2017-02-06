2158b53c0902436d29e811da5aaffee6450ccda7
Shekar <amshekar@hotmail.com>
2017-02-06 09:24:06 +0530

    First solution commit

---
+++ uui-seed/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="uui_seed.MvcApplication" Language="C#" %>
---
+++ uui-seed/app/app.module.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    'use strict';
+
+    angular.module(
+        'capability', [
+            //3rd party dep
+            'ui.router'
+        ])
+    //.config(function ($stateProvider, $urlRouterProvider) {
+    //    $urlRouterProvider.otherwise('/');
+
+    //    $stateProvider
+    //        .state('home', {
+    //            url: '/',
+    //            templateUrl: 'app/home/partial-home.html'
+    //        })
+    //     .state('dashboard', {
+    //         url: '/dashboard',
+    //         templateUrl: 'app/dashboard/partial-dashboard.html',
+    //         controller: 'DashboardController'
+    //     })
+    //    .state('associate', {
+    //        url: '/associate',
+    //        templateUrl: 'app/associates/partial-associate.html',
+    //        controller: 'AssociateController'
+    //    })
+    //     .state('training', {
+    //         url: '/training',
+    //         templateUrl: 'app/trainings/partial-training.html',
+    //         controller: 'TrainingController'
+    //     })
+    //     .state('settings', {
+    //         url: '/settings',
+    //         templateUrl: 'app/settings/partial-settings.html',
+    //         controller: 'SettingsController'
+    //     })
+    //     .state('export', {
+    //         url: '/export',
+    //         templateUrl: 'app/exports/partial-export.html',
+    //          controller: 'ExportController'
+    //     });
+
+
+
+    //});
+})();
